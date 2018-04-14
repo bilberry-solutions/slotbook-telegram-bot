6d75e7e2e089055d373b1c0712737e12da6f2522
Andrey Parkhomenko <andrey.parkhomenko@nextiva.com>
2018-04-14 15:19:19 +0300

    Initial

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "slotbook-telegram-bot"
+
+version := "1.0"
+
+scalaVersion := "2.12.5"
+
+libraryDependencies += "info.mukel" %% "telegrambot4s" % "3.0.14"
---
+++ src/main/scala/me/slotbook/client/telegram/SlotbookBot.scala
@@ -0,0 +1,7 @@
+package me.slotbook.client.telegram
+
+object SlotbookBot extends App{
+  override def main(args: Array[String]): Unit = {
+    println("Test")
+  }
+}
